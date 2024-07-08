82589b6c168b28eb328919923f5dc49475750580
Hamdan <hamdan.hy@gmail.com>
2024-07-08 17:57:45 +0800

    Reformat code

--- agm/app.R
+++ agm/app.R
@@ -19,7 +19,7 @@ ui <- grid_page(
     "area0 area0"
   ),
   row_sizes = c(
-    "255px",
+    "240px",
     "1fr"
   ),
   col_sizes = c(
@@ -31,102 +31,102 @@ ui <- grid_page(
     area = "area0",
     card_body(
       conditionalPanel(
-                                                  condition = "output.nama > ''",
-                                                  tabsetPanel(
-                                                    nav_panel(
-                                                      title = "Kata-kata Aluan Presiden",
-                                                      card(
-                                                        full_screen = TRUE,
-                                                        card_body(
-                                                          tags$iframe(style="height:300px; width:70%", src="cny.mp4"),
-                                                          selectInput(
-                                                            inputId = "introIn",
-                                                            label = "Kandungan",
-                                                            choices = list(
-                                                              "Memuaskan" = "Memuaskan",
-                                                              "Sederhana" = "Sederhana",
-                                                              "Tidak berkenaan" = "Tidak berkenaan"
-                                                            )
-                                                          ),
-                                                          textInput(
-                                                            inputId = "introText",
-                                                            label = "Komen dan Ulasan",
-                                                            value = "Komen anda ..."
-                                                          )
-                                                        )
-                                                      )
-                                                    ),
-                                                    nav_panel(
-                                                      title = "Laporan Tahunan",
-                                                      card(
-                                                        full_screen = TRUE,
-                                                        card_body(
-                                                          tags$iframe(style="height:600px; width:100%", src="report.pdf"),
-                                                          selectInput(
-                                                            inputId = "yrpIn",
-                                                            label = "Kandungan",
-                                                            choices = list(
-                                                              "Memuaskan" = "Memuaskan",
-                                                              "Sederhana" = "Sederhana",
-                                                              "Tidak berkenaan" = "Tidak berkenaan"
-                                                            )
-                                                          ),
-                                                          textInput(
-                                                            inputId = "yrpText",
-                                                            label = "Komen dan Ulasan",
-                                                            value = "Komen anda ..."
-                                                          )
-                                                        )
-                                                      )
-                                                    ),
-                                                    nav_panel(
-                                                      title = "Laporan Akaun",
-                                                      card(
-                                                        full_screen = TRUE,
-                                                        card_body(
-                                                          tags$iframe(style="height:600px; width:100%", src="akaun.pdf"),
-                                                          selectInput(
-                                                            inputId = "annIn",
-                                                            label = "Kandungan",
-                                                            choices = list(
-                                                              "Memuaskan" = "Memuaskan",
-                                                              "Sederhana" = "Sederhana",
-                                                              "Tidak berkenaan" = "Tidak berkenaan"
-                                                            )
-                                                          ),
-                                                          textInput(
-                                                            inputId = "annText",
-                                                            label = "Komen dan Ulasan",
-                                                            value = "Komen anda ..."
-                                                          )
-                                                        )
-                                                      )
-                                                    ),
-                                                    nav_panel(
-                                                      title = "Minit MAT ke-13",
-                                                      card(
-                                                        full_screen = TRUE,
-                                                        card_body(
-                                                          tags$iframe(style="height:600px; width:100%", src="minit_agm.pdf"),
-                                                          selectInput(
-                                                            inputId = "momIn",
-                                                            label = "Kandungan",
-                                                            choices = list(
-                                                              "Memuaskan" = "Memuaskan",
-                                                              "Sederhana" = "Memuaskan",
-                                                              "Tidak berkenaan" = "Tidak berkenaan"
-                                                            )
-                                                          ),
-                                                          textInput(
-                                                            inputId = "momText",
-                                                            label = "Komen dan Ulasan",
-                                                            value = "Komen anda ..."
-                                                          )
-                                                        )
-                                                      )
-                                                    )
-                                                  )
-                                                )
+        condition = "output.nama > ''",
+        tabsetPanel(
+          nav_panel(
+            title = "Kata-kata Aluan Presiden",
+            card(
+              full_screen = TRUE,
+              card_body(
+                tags$iframe(style="height:300px; width:70%", src="cny.mp4"),
+                selectInput(
+                  inputId = "introIn",
+                  label = "Kandungan",
+                  choices = list(
+                    "Memuaskan" = "Memuaskan",
+                    "Sederhana" = "Sederhana",
+                    "Tidak berkenaan" = "Tidak berkenaan"
+                  )
+                ),
+                textInput(
+                  inputId = "introText",
+                  label = "Komen dan Ulasan",
+                  value = "Komen anda ..."
+                )
+              )
+            )
+          ),
+          nav_panel(
+            title = "Laporan Tahunan",
+            card(
+              full_screen = TRUE,
+              card_body(
+                tags$iframe(style="height:600px; width:100%", src="report.pdf"),
+                selectInput(
+                  inputId = "yrpIn",
+                  label = "Kandungan",
+                  choices = list(
+                    "Memuaskan" = "Memuaskan",
+                    "Sederhana" = "Sederhana",
+                    "Tidak berkenaan" = "Tidak berkenaan"
+                  )
+                ),
+                textInput(
+                  inputId = "yrpText",
+                  label = "Komen dan Ulasan",
+                  value = "Komen anda ..."
+                )
+              )
+            )
+          ),
+          nav_panel(
+            title = "Laporan Akaun",
+            card(
+              full_screen = TRUE,
+              card_body(
+                tags$iframe(style="height:600px; width:100%", src="akaun.pdf"),
+                selectInput(
+                  inputId = "annIn",
+                  label = "Kandungan",
+                  choices = list(
+                    "Memuaskan" = "Memuaskan",
+                    "Sederhana" = "Sederhana",
+                    "Tidak berkenaan" = "Tidak berkenaan"
+                  )
+                ),
+                textInput(
+                  inputId = "annText",
+                  label = "Komen dan Ulasan",
+                  value = "Komen anda ..."
+                )
+              )
+            )
+          ),
+          nav_panel(
+            title = "Minit MAT ke-13",
+            card(
+              full_screen = TRUE,
+              card_body(
+                tags$iframe(style="height:600px; width:100%", src="minit_agm.pdf"),
+                selectInput(
+                  inputId = "momIn",
+                  label = "Kandungan",
+                  choices = list(
+                    "Memuaskan" = "Memuaskan",
+                    "Sederhana" = "Memuaskan",
+                    "Tidak berkenaan" = "Tidak berkenaan"
+                  )
+                ),
+                textInput(
+                  inputId = "momText",
+                  label = "Komen dan Ulasan",
+                  value = "Komen anda ..."
+                )
+              )
+            )
+          )
+        )
+      )
     )
   ),
   grid_card(
